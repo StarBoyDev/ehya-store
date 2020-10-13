228e17f8d5587656c99ab9313b5c606151518e73
Taras <taraskashanyk@ukr.net>
2020-10-14 01:58:21 +0300

    create: header and responsive & modal login

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+  var modalButton = $("[data-toggle=modal]");
+  var closeModalButton = $(".modal__close");
+  modalButton.on("click", openModal);
+  closeModalButton.on("click", closeModal);
+
+  function openModal() {
+    var modalOverlay = $(".modal__overlay");
+    var modalMain = $(".modal__main");
+    modalOverlay.addClass("modal__overlay--visible");
+    modalMain.addClass("modal__main--visible");
+  }
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalMain = $(".modal__main");
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalMain.removeClass("modal__main--visible");
+  }
+});

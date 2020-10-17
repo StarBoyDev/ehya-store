343ce148b477dbf51ad7a16dda3e0b0c619b51a7
Taras <taraskashanyk@ukr.net>
2020-10-18 02:46:24 +0300

    create: newsletter section, added php mailer

---
+++ send.php
@@ -0,0 +1,53 @@
+<?php
+// Файлы phpmailer
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+// Переменные, которые отправляет пользователь
+$email = $_POST['email'];
+
+
+// Формирование самого письма
+$title = "Новое обращение Ehya";
+$body = "
+<h2>Новое обращение</h2>
+<b>Имя:</b> $email<br>
+";
+
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $mail->isSMTP();   
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+   //  $mail->SMTPDebug = 2;
+    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.ukr.net'; // SMTP сервера вашей почты
+    $mail->Username   = 'taraswebdev@ukr.net'; // Логин на почте
+    $mail->Password   = 'nryyW7mSlD080RHh'; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->setFrom('taraswebdev@ukr.net', 'taraswebdev.top'); // Адрес самой почты и имя отправителя
+
+    // Получатель письма
+    $mail->addAddress('taraskashanyk@ukr.net');  
+
+// Отправка сообщения
+$mail->isHTML(true);
+$mail->Subject = $title;
+$mail->Body = $body;    
+
+// Проверяем отравленность сообщения
+if ($mail->send()) {$result = "success";} 
+else {$result = "error";}
+
+} catch (Exception $e) {
+    $result = "error";
+    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
+
+// Отображение результата
+header('Location: thank-you.html');
